6af819dce61da1539e5d40681a169f3d1b99b9ce
hoanghungnguyen <hoanghunghm2004@gmail.com>
2024-08-01 17:25:09 +0700

    navbar bootstrap admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -29,7 +29,7 @@ if ($logout == 'dangxuat') {
 <body>
     <p>Xin chào : <?php echo $_SESSION['btn-login']; ?> <a href="?login=dangxuat">Đăng xuất</a></p>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#"><img style="height: 35px; width: 35px;" src="../images/logoshop.JPG" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -37,24 +37,24 @@ if ($logout == 'dangxuat') {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Đơn hàng <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Danh mục</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
+                        Sản phẩm
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Action</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
+                    </div> -->
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link " href="#">Khách hàng</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
