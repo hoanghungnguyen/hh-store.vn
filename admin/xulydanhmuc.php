94cea465abf75b22f075fde0a6b86a02704ae1e4
hoanghungnguyen <hoanghunghm2004@gmail.com>
2024-08-01 18:19:26 +0700

    done xulydanhmuc update and delete

--- admin/xulydanhmuc.php
+++ admin/xulydanhmuc.php
@@ -16,32 +16,30 @@ require("../db/connect.php");
     <div class="container">
         <div class="row">
             <?php
-            if (isset($_POST['quanly'])) {
-                $capnhat = $_POST['quanly'];
+            if (isset($_GET['quanly'])) {
+                $capnhat = $_GET['quanly'];
             } else {
                 $capnhat = "";
             }
             if ($capnhat == 'update') {
             ?>
-            <div class="col-md-4">
-                <h4>Cập nhật danh mục</h4>
-                <label for="">Tên danh mục</label>
-                <form action="" method="POST">
-                    <input type="text" name="danhmuc" class="form-control" placeholder="Tên danh mục">
-                    <input style="margin-top: 12px;" type="submit" name="themdanhmuc" class="btn btn-default"
-                        value="Thêm danh mục">
-                </form>
-            </div>
+                <div class="col-md-4">
+                    <h4>Cập nhật danh mục</h4>
+                    <label for="">Tên danh mục</label>
+                    <form action="" method="POST">
+                        <input type="text" name="danhmuc" class="form-control" placeholder="Tên danh mục">
+                        <input style="margin-top: 12px;" type="submit" name="themdanhmuc" class="btn btn-default" value="Thêm danh mục">
+                    </form>
+                </div>
             <?php } else { ?>
-            <div class="col-md-4">
-                <h4>Thêm danh mục</h4>
-                <label for="">Tên danh mục</label>
-                <form action="" method="POST">
-                    <input type="text" name="danhmuc" class="form-control" placeholder="Tên danh mục">
-                    <input style="margin-top: 12px;" type="submit" name="themdanhmuc" class="btn btn-default"
-                        value="Thêm danh mục">
-                </form>
-            </div>
+                <div class="col-md-4">
+                    <h4>Thêm danh mục</h4>
+                    <label for="">Tên danh mục</label>
+                    <form action="" method="POST">
+                        <input type="text" name="danhmuc" class="form-control" placeholder="Tên danh mục">
+                        <input style="margin-top: 12px;" type="submit" name="themdanhmuc" class="btn btn-default" value="Thêm danh mục">
+                    </form>
+                </div>
             <?php } ?>
             <div class="col-md-8">
                 <h4>Liệt kê danh mục</h4>
@@ -59,12 +57,11 @@ require("../db/connect.php");
                     while ($row_danhmuc = mysqli_fetch_array($sql_danhmuc)) {
                         $tt++;
                     ?>
-                    <tr>
-                        <td><?php echo $tt ?></td>
-                        <td><?php echo $row_danhmuc['category_name']; ?></td>
-                        <td><a href="?quanly=xoa&id=<?php echo $row_danhmuc['category_id'] ?>">Xóa</a> || <a
-                                href="?quanly=update&id=<?php echo $row_danhmuc['category_id'] ?>">Cập nhật</a></td>
-                    </tr>
+                        <tr>
+                            <td><?php echo $tt ?></td>
+                            <td><?php echo $row_danhmuc['category_name']; ?></td>
+                            <td><a href="?quanly=xoa&id=<?php echo $row_danhmuc['category_id'] ?>">Xóa</a> || <a href="?quanly=update&id=<?php echo $row_danhmuc['category_id'] ?>">Cập nhật</a></td>
+                        </tr>
                     <?php } ?>
                 </table>
             </div>
