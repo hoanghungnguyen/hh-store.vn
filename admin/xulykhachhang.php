296067915b2960cdf673cceeff062c806ce3f0b1
hoanghungnguyen <hoanghunghm2004@gmail.com>
2024-08-05 17:10:33 +0700

    done

--- admin/xulykhachhang.php
+++ admin/xulykhachhang.php
@@ -55,7 +55,7 @@ require("../db/connect.php");
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="?">Đơn hàng <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="xulydonhang.php">Đơn hàng <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="xulydanhmuc.php">Danh mục</a>
