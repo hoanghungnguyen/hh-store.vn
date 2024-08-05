812720509d6022058c24bed85c82051ec5e95276
hoanghungnguyen <hoanghunghm2004@gmail.com>
2024-08-05 16:13:53 +0700

    show data chitiet don hang da giao dich

--- include/xemdonhang.php
+++ include/xemdonhang.php
@@ -26,25 +26,98 @@
                                 $sql_select = mysqli_query($con, "SELECT * FROM tbl_giaodich WHERE tbl_giaodich.khachhang_id='$id_khachhang' GROUP BY tbl_giaodich.magiaodich");
 
                                 ?>
-                                <table class="table table-bordered">
+                                <table class="table table-bordered ">
                                     <tr>
-                                        <td>Thứ tự</td>
-                                        <td>Mã giao dịch</td>
-                                        <td>Ngày đặt</td>
+                                        <th>Thứ tự</th>
+                                        <th>Mã giao dịch</th>
+
+                                        <th>Ngày đặt</th>
+                                        <th>Quản lý</th>
+                                        <th>Tình trạng</th>
+                                        <th>Yêu cầu</th>
                                     </tr>
                                     <?php
-                                    $tt = 0;
-                                    while ($row_giaodich = mysqli_fetch_array($sql_select)) {
-                                        $tt++;
+                                    $i = 0;
+                                    while ($row_donhang = mysqli_fetch_array($sql_select)) {
+                                        $i++;
                                     ?>
+                                    <tr>
+                                        <td><?php echo $i; ?></td>
+                                        <td><?php echo $row_donhang['magiaodich']; ?></td>
+                                        <td><?php echo $row_donhang['ngaythang'] ?></td>
+                                        <td><a
+                                                href="?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>&magiaodich=<?php echo $row_donhang['magiaodich'] ?>">Xem
+                                                chi tiết</a></td>
+                                        <td><?php
+                                                if ($row_donhang['tinhtrangdon'] == 0) {
+                                                    echo 'Đã đặt hàng';
+                                                } else {
+                                                    echo 'Đã xử lý | Đang giao hàng';
+                                                }
+                                                ?></td>
+                                        <td>
+                                            <?php
+                                                if ($row_donhang['huydon'] == 0) {
+                                                ?>
+                                            <a
+                                                href="index.php?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>&magiaodich=<?php echo $row_donhang['magiaodich'] ?>&huydon=1">Yêu
+                                                cầu hủy</a>
+                                            <?php
+                                                } elseif ($row_donhang['huydon'] == 1) {
+                                                ?>
+                                            <p>Đang chờ hủy...</p>
+                                            <?php
+                                                } else {
+                                                    echo 'Đã hủy';
+                                                }
+                                                ?>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </table>
+
+                                <div class="col-md-12">
+                                    <p>Chi tiết đơn hàng</p><br>
+                                    <?php
+                                    if (isset($_GET['magiaodich'])) {
+                                        $magiaodich = $_GET['magiaodich'];
+                                    } else {
+                                        $magiaodich = '';
+                                    }
+                                    $sql_select = mysqli_query($con, "SELECT * FROM tbl_giaodich,tbl_khachhang,tbl_sanpham WHERE tbl_giaodich.sanpham_id=tbl_sanpham.sanpham_id AND tbl_khachhang.khachhang_id=tbl_giaodich.khachhang_id AND tbl_giaodich.magiaodich='$magiaodich' ORDER BY tbl_giaodich.giaodich_id DESC");
+                                    ?>
+                                    <table class="table table-bordered ">
                                         <tr>
-                                            <td><?php echo $tt ?></td>
-                                            <td><?php echo $row_giaodich['magiaodich']; ?></td>
-                                            <td><?php echo $row_giaodich['ngaythang']; ?></td>
+                                            <th>Thứ tự</th>
+                                            <th>Mã giao dịch</th>
+                                            <th>Tên sản phẩm</th>
+                                            <th>Số lượng</th>
+                                            <th>Ngày đặt</th>
 
                                         </tr>
-                                    <?php } ?>
-                                </table>
+                                        <?php
+                                        $i = 0;
+                                        while ($row_donhang = mysqli_fetch_array($sql_select)) {
+                                            $i++;
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $i; ?></td>
+
+                                            <td><?php echo $row_donhang['magiaodich']; ?></td>
+
+                                            <td><?php echo $row_donhang['sanpham_name']; ?></td>
+
+                                            <td><?php echo $row_donhang['soluong']; ?></td>
+
+                                            <td><?php echo $row_donhang['ngaythang'] ?></td>
+                                        </tr>
+                                        <?php
+                                        }
+                                        ?>
+                                    </table>
+                                </div>
                             </div>
                         </div>
 
